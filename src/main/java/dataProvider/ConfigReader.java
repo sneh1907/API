67724ce7361947428433c75a2f3cdd943ff324a0
sneh1907 <Snehataneja1907@gmail.com>
2023-04-27 13:41:18 +0530

    chnages done

---
+++ src/main/java/dataProvider/ConfigReader.java
@@ -0,0 +1,30 @@
+package dataProvider;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    private static Properties propertiesObject;
+
+    public static String getURL() {
+        String configFilePath = System.getProperty("user.dir");
+        Properties configProperties = ConfigReader.readPropertiesFile(configFilePath + "//configuration.properties");
+        return configProperties.getProperty("baseURL");
+    }
+
+    public static Properties readPropertiesFile(String fileName) {
+
+        try {
+            FileInputStream fileStream = new FileInputStream(fileName);
+            propertiesObject = new Properties();
+            propertiesObject.load(fileStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return propertiesObject;
+    }
+}
--- src/test/java/apiAssignmentOne/ProductApi.java
+++ src/test/java/apiAssignmentOne/ProductApi.java
@@ -1,57 +1,67 @@
 package apiAssignmentOne;
 
-import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import org.testng.Assert;
+import dataProvider.*;
+import io.restassured.*;
+import io.restassured.path.json.*;
+import io.restassured.response.*;
 import org.testng.annotations.Test;
-import static io.restassured.RestAssured.given;
+
+import static io.restassured.RestAssured.*;
+
+import org.testng.Assert;
+
+import java.util.logging.*;
 
 public class ProductApi {
-    @Test(priority = 0)
-    public void testgetcall() {
-        Response response = given().when().get("https://automationexercise.com/api/productsList");
-        var statusCode = response.getStatusCode();
-        System.out.println("Actual status code is " + statusCode);
-        Assert.assertEquals(statusCode, 200);
-        String responseBody = response.getBody().asString();
-        Assert.assertTrue(responseBody.contains("Blue Top"));
-        JsonPath jsonResponse = new JsonPath(response.asString());
-        var idLength = jsonResponse.getInt("products.id.size()");
-        System.out.println("The length is : " + idLength);
+    private String url = "";
 
+    private Logger logger = Logger.getLogger(ProductApi.class.getName());
+
+    @Test(priority = 0)
+    public void testGetcall() {
+        url = ConfigReader.getURL();
+        Response response = get(url);
+        var statuscode = response.getStatusCode();
+        //  Assert.assertEquals(statusCode, 200);
+        logger.log(Level.INFO, "get status code " + statuscode);
     }
 
     @Test(priority = 1)
-    public void testPostRequest() {
-        Response response = given().when().post("https://automationexercise.com/api/productsList");
-        var statusCode = response.getStatusCode();
-        System.out.println("Actual status code is " + statusCode);
-        Assert.assertEquals(statusCode, 200);
-        String responseBody = response.getBody().asString();
-        Assert.assertTrue(responseBody.contains("This request method is not supported."));
+    public void validateList() {
+        url = ConfigReader.getURL();
+        var getALLProductList = given().when().get(url).then().log().all().toString();
+        logger.log(Level.INFO, "List return by API is " + getALLProductList);
     }
 
     @Test(priority = 2)
-    public void testGetAllBrandsList() {
-        Response response = RestAssured.given().when().get("https://automationexercise.com/api/brandsList");
+    public void validateContent() {
+        url = ConfigReader.getURL();
+        Response response = RestAssured.given().when().get(url);
         var statusCode = response.getStatusCode();
-        System.out.println("Actual status code is " + statusCode);
-        Assert.assertEquals(statusCode, 200);
-        String responseBody = response.getBody().asString();
-        Assert.assertTrue(responseBody.contains("{\"id\": 33, \"brand\": \"Polo\"}"));
-        JsonPath jsonPath = response.jsonPath();
-        var responseLength = jsonPath.getList("brands").size();
-        Assert.assertTrue(responseLength > 0);
+        JsonPath jsonPath = new JsonPath(response.asString());
+        logger.info("my json response: " + jsonPath.get("products"));
+        var productsArray = jsonPath.get("products");
+        var idLength = jsonPath.getInt("products.id.size()");
+        for (int counter = 0; counter < productsArray.toString().length(); counter++) {
+            String productName = jsonPath.getString("products[" + counter + "].name");
+            logger.log(Level.INFO, "Product name is  " + productName);
+            if (productName == "Cotton V-Neck T-Shirt") {
+                Assert.assertEquals(productName, "Cotton V-Neck T-Shirt");
+                Assert.assertEquals(jsonPath.getString("products[" + counter + "].price"), "Rs. 400");
+                Assert.assertEquals(jsonPath.getString("products[" + counter + "].brand"), "H&M");
+                Assert.assertEquals(jsonPath.getString("products[" + counter + "].category.category"), "Top");
+                logger.log(Level.INFO, "Product name is  " + productName);
+            }
+        }
     }
 
     @Test(priority = 3)
     public void testPutRequest() {
-        Response response = given().when().post("https://automationexercise.com/api/brandsList");
-        var statusCode = response.getStatusCode();
-        System.out.println("Actual status code is " + statusCode);
-        Assert.assertEquals(statusCode, 200);
-        String responseBody = response.getBody().asString();
-        Assert.assertTrue(responseBody.contains("This request method is not supported."));
+        url = ConfigReader.getURL();
+        var response = given().when().get(url).then().extract().asString();
+        JsonPath jsonResponse = new JsonPath(response);
+        var idLength = jsonResponse.getInt("products.id.size()");
+        Assert.assertEquals(34, idLength);
+        logger.log(Level.INFO, "Length is " + idLength);
     }
 }
